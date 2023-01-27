9efb42627d1466122d05395e4e8462addc5d55e3
Tiimecmy <timeizstop@gmail.com>
2023-01-28 00:49:15 +0700

    first commit

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import CardItem from './CardItem'
 import './Card.css'
+import imG1 from '../images/img-1.jpg'
+import imG2 from '../images/img-2.jpg'
+import imG3 from '../images/img-3.jpg'
+import imG4 from '../images/img-4.jpg'
+import imG8 from '../images/img-8.jpg'
 
 function Card() {
   return (
@@ -10,13 +15,13 @@ function Card() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem 
-              src='/images/img-9.jpg'
+              src={imG1}
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path='/services'
             />
             <CardItem 
-              src='/images/img-2.jpg'
+              src={imG2}
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
@@ -24,19 +29,19 @@ function Card() {
           </ul>
           <ul className="cards__items">
             <CardItem 
-              src='/images/img-3.jpg'
+              src={imG3}
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem 
-              src='/images/img-4.jpg'
+              src={imG4}
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path='/services'
             />
             <CardItem 
-              src='/images/img-8.jpg'
+              src={imG8}
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adventure'
               path='/services'
